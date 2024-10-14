70719fc83d1df254232c484e185ac37a9aea9619
Nite5ky <aakashyadav@lampros.tech>
2024-10-14 11:30:00 +0530

    fix: utils, naming and imports

---
+++ utils/keeperConfig.js
@@ -0,0 +1,24 @@
+const keeperConfig = {
+    "1": {
+        "privateKey": "2d59f7ee79f234bcca8b64956b4ac4cedbc5d961d22b08c2e237a53bb92328e4",
+        "port": 3001,
+        "name": "Frodo"
+    },
+    "2": {
+        "privateKey": "761bbd951bfe044c5c9a1a8458e6948acb0f8ff5a8cc6a3ac25dfadde17897c6",
+        "port": 3002,
+        "name": "Sam"
+    },
+    "3": {
+        "privateKey": "e35035d5ac2ec8f0e979bf9291ab4876315be30ea747a6619352a4041384c83c",
+        "port": 3003,
+        "name": "Merry"
+    },
+    "4": {
+        "privateKey": "b3f7360cae15bb9a31a396f304d79313e0f00d0c6ddd981b359cffe12ec14e3a",
+        "port": 3004,
+        "name": "Pippin"
+    }
+}
+
+module.exports = { keeperConfig };
