98533a9cdd95429f3cd45332acd16ca89f43230e
Nite5ky <aakashyadav@lampros.tech>
2024-11-27 14:28:53 +0530

    refactor: go pkg path

--- pkg/core/crypto/keystore.go
+++ pkg/core/crypto/keystore.go
@@ -14,7 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	ecrypto "github.com/ethereum/go-ethereum/crypto"
 	"github.com/google/uuid"
-	"github.com/trigg3rX/triggerx-keeper/pkg/logger"
+	"github.com/trigg3rX/triggerx-keeper/pkg/core/logger"
 	"go.uber.org/zap"
 )
 
