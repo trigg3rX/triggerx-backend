a49b74a8d815016c5432f533f8da5a206ae7b972
Nite5ky <aakashyadav@lampros.tech>
2024-12-02 14:29:10 +0530

    resolve: mod file

--- config/config.go
+++ config/config.go
@@ -1,45 +1,18 @@
 package config
 
 import (
-    "log"
-    "os"
-
-    "github.com/trigg3rX/go-backend/pkg/types"
+	"github.com/spf13/viper"
+    "github.com/trigg3rX/eigensdk-go/types"
 )
 
-type KeeperConfig struct {
-    Name            string
-    WorkerCount     int
-    MaxConcurrentJobs int
-    LogDirectory    string
-    JobTypes        []string
-}
-
-func LoadConfig() *KeeperConfig {
-    // Default configuration
-    config := &KeeperConfig{
-        Name:            "keeper-default",
-        WorkerCount:     4,
-        MaxConcurrentJobs: 10,
-        LogDirectory:    "./logs",
-        JobTypes:        []string{"default"},
+func LoadConfig() (*types.QuorumNum, error) {
+    viper.SetConfigFile("triggerx_operator.yaml")
+    if err := viper.ReadInConfig(); err != nil {
+        return nil, err
     }
-
-    // Create log directory if it doesn't exist
-    if err := os.MkdirAll(config.LogDirectory, 0755); err != nil {
-        log.Printf("Failed to create log directory: %v", err)
+    var config types.QuorumNum
+    if err := viper.Unmarshal(&config); err != nil {
+        return nil, err
     }
-
-    return config
-}
-
-// ValidateJob checks if the job is compatible with this keeper
-func (c *KeeperConfig) ValidateJob(job *types.Job) bool {
-    // Simple job type validation
-    for _, supportedType := range c.JobTypes {
-        if supportedType == "default" || supportedType == job.ArgType {
-            return true
-        }
-    }
-    return false
+    return &config, nil
 }
