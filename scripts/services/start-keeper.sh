b2dd1ad2e8a7e823d0db3f2056777b33d04f083f
Nite5ky <aakashyadav@lampros.tech>
2025-03-30 17:41:31 +0530

    fix: updated grafana dashboard

--- scripts/services/start-keeper.sh
+++ scripts/services/start-keeper.sh
@@ -9,7 +9,9 @@ KEEPER_PID=$!
 echo "Starting othentic-cli..."
 othentic-cli node attester \
     /ip4/157.173.218.229/tcp/9876/p2p/12D3KooWBNFG1QjuF3UKAKvqhdXcxh9iBmj88cM5eU2EK5Pa91KB \
-    --metrics --metrics.port 7070\
+    --metrics \
+    --metrics.export-url http://157.173.218.229:9091 \
+    --p2p.datadir data/peer_registry \
     --avs-webapi http://127.0.0.1 \
     --avs-webapi-port 9005 \
     --json-rpc \
