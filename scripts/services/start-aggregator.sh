fd0d309f3ee6d43aa53b8b08f394f5dc3f59102a
Nite5ky <aakashyadav@lampros.tech>
2025-03-31 20:15:31 +0530

    fix: aggregator script for persistent storage

--- scripts/services/start-aggregator.sh
+++ scripts/services/start-aggregator.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
+source .env
+
 # Delay in ms for the aggregator to wait before submitting the task to the EigenLayer
 DELAY=10000
 
 # Sync interval in ms for the aggregator to run internal tasks
 SYNC_INTERVAL=7200000
 
-# Metrics port, default 6060
-METRICS_PORT=6060
-
-# Metrics export url
-METRICS_EXPORT_URL=
-
 othentic-cli node aggregator \
     --json-rpc \
     --json-rpc.port $AGGREGATOR_RPC_PORT \
     --p2p.port $AGGREGATOR_P2P_PORT \
+    --p2p.datadir data/peerstore \
     --internal-tasks \
     --metrics \
     --delay $DELAY \
