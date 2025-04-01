cd29c46259c18970604c647a11fb49ef58db640a
Nite5ky <aakashyadav@lampros.tech>
2025-04-01 18:35:34 +0530

    fix: minor config update

--- scripts/services/start-keeper.sh
+++ scripts/services/start-keeper.sh
@@ -24,8 +24,7 @@ echo "Starting othentic-cli..."
 othentic-cli node attester \
     /ip4/192.168.1.57/tcp/9876/p2p/12D3KooWBNFG1QjuF3UKAKvqhdXcxh9iBmj88cM5eU2EK5Pa91KB \
     --metrics \
-    # --metrics.export-url http://192.168.1.57:9091 \
-    --p2p.datadir data/peer_registry \
+    --p2p.datadir peerstore \
     --avs-webapi http://127.0.0.1 \
     --avs-webapi-port 9005 \
     --json-rpc \
