bb7896583f05f47bcb5f8c512660a013732c1240
prapti1211 <praptishah@lampros.tech>
2025-04-01 12:08:26 +0000

    feat: dev-keeper v0.0.5

--- scripts/services/start-keeper.sh
+++ scripts/services/start-keeper.sh
@@ -10,7 +10,6 @@ echo "Starting othentic-cli..."
 othentic-cli node attester \
     /ip4/157.173.218.229/tcp/9876/p2p/12D3KooWBNFG1QjuF3UKAKvqhdXcxh9iBmj88cM5eU2EK5Pa91KB \
     --metrics \
-    --metrics.export-url http://157.173.218.229:9091 \
     --p2p.datadir data/peer_registry \
     --avs-webapi http://127.0.0.1 \
     --avs-webapi-port 9005 \
