7c053ea63158ec31256fd7d526fa6af59f242093
5kyguy <aakashyadav@lampros.tech>
2025-06-01 18:41:39 +0530

    fix(pkg/env): add logs for better debug

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,6 +11,7 @@ func GetEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
+	fmt.Printf("Environment variable %s not found, using default value: %s\n", key, defaultValue)
 	return defaultValue
 }
 
@@ -17,9 +19,11 @@ func GetEnvBool(key string, defaultValue bool) bool {
 	if value, exists := os.LookupEnv(key); exists {
 		boolValue, err := strconv.ParseBool(value)
 		if err != nil {
+			fmt.Printf("Environment variable %s not found, using default value: %t\n", key, defaultValue)
 			return defaultValue
 		}
 		return boolValue
 	}
+	fmt.Printf("Environment variable %s not found, using default value: %t\n", key, defaultValue)
 	return defaultValue
 }
