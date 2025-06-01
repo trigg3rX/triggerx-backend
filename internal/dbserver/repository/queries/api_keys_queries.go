72800e2c73cf6a0f4ff36058d119bb8267c6d6a2
5kyguy <aakashyadav@lampros.tech>
2025-06-01 17:49:43 +0530

    feat(dbserver/repository/queries): migrate to repository package and arranged them to create / update / get data format

---
+++ internal/dbserver/repository/queries/api_keys_queries.go
@@ -0,0 +1,54 @@
+package queries
+
+// Create Queries
+const (
+	CreateApiKeyQuery = `
+			INSERT INTO triggerx.apikeys (key, owner, is_active, rate_limit, last_used, created_at)
+			VALUES (?, ?, ?, ?, ?, ?)`
+)
+
+// Update Queries
+const (
+	UpdateApiKeyQuery = `
+			UPDATE triggerx.apikeys 
+			SET is_active = ?, rate_limit = ? 
+			WHERE key = ?`
+
+	UpdateApiKeyStatusQuery = `
+			UPDATE triggerx.apikeys 
+			SET is_active = ? 
+			WHERE key = ?`
+
+	UpdateApiKeyLastUsedQuery = `
+			UPDATE triggerx.apikeys 
+			SET last_used = ?, success_count = ?, failed_count = ? 
+			WHERE key = ?`
+)
+
+// Read Queries
+const (
+	GetApiKeyDataByOwnerQuery = `
+			SELECT key, owner, is_active, rate_limit, success_count, failed_count, last_used, created_at 
+			FROM triggerx.apikeys 
+			WHERE owner = ? ALLOW FILTERING`
+
+	GetApiKeyDataByApiKeyQuery = `
+			SELECT key, owner, is_active, rate_limit, success_count, failed_count, last_used, created_at 
+			FROM triggerx.apikeys 
+			WHERE key = ? ALLOW FILTERING`
+
+	GetApiKeyCallCountQuery = `
+			SELECT success_count, failed_count 
+			FROM triggerx.apikeys 
+			WHERE key = ? ALLOW FILTERING`
+
+	GetApiKeyByOwnerQuery = `
+			SELECT key
+			FROM triggerx.apikeys 
+			WHERE owner = ? ALLOW FILTERING`
+
+	GetApiOwnerByApiKeyQuery = `
+			SELECT owner
+			FROM triggerx.apikeys 
+			WHERE key = ? ALLOW FILTERING`
+)
---
+++ internal/dbserver/repository/queries/job_queries.go
@@ -0,0 +1,51 @@
+package queries
+
+// Create Queries
+const (
+	GetMaxJobIDQuery = `SELECT MAX(job_id) FROM triggerx.job_data`
+
+	CreateJobDataQuery = `
+			INSERT INTO triggerx.job_data (
+				job_id, job_title, task_definition_id, user_id, link_job_id, chain_status,
+				custom, time_frame, recurring, status, job_cost_prediction,
+				created_at, updated_at, timezone
+			) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	// 14 values to be inserted, so 14 ?s
+)
+
+// Write Queries
+const (
+	UpdateJobDataFromUserQuery = `
+			UPDATE triggerx.job_data 
+			SET job_title = ?, time_frame = ?, recurring = ?, status = ?,
+			job_cost_prediction = ?, updated_at = ?
+			WHERE job_id = ?`
+	
+	UpdateJobDataLastExecutedAtQuery = `
+			UPDATE triggerx.job_data 
+			SET task_ids = ?, job_cost_actual = ?, last_executed_at = ?
+			WHERE job_id = ?`
+
+	UpdateJobDataStatusQuery = `
+			UPDATE triggerx.job_data 
+			SET status = ?, updated_at = ?
+			WHERE job_id = ?`
+)
+
+// Read Queries
+const (
+	GetJobDataByJobIDQuery = `
+			SELECT job_id, job_title, task_definition_id, user_id, link_job_id, chain_status,
+				custom, time_frame, recurring, status, job_cost_prediction, job_cost_actual,
+				task_ids, created_at, updated_at, last_executed_at, timezone
+			FROM triggerx.job_data 
+			WHERE job_id = ?`
+
+	GetTaskDefinitionIDByJobIDQuery = `
+			SELECT task_definition_id FROM triggerx.job_data 
+			WHERE job_id = ?`
+
+	GetTaskIDsByJobIDQuery = `
+			SELECT task_ids FROM triggerx.job_data 
+			WHERE job_id = ?`
+)
