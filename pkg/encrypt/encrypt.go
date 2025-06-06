b137f21c7aacfe93adcf8dc17af217793ce6b6e8
5kyguy <aakashyadav@lampros.tech>
2025-06-06 22:47:09 +0530

    feat (pkg/encrypt): add signJSONMessage function which shall help in validation process
    used by schedulers, perfomers and validators

--- pkg/encrypt/encrypt.go
+++ pkg/encrypt/encrypt.go
@@ -9,24 +9,6 @@ import (
 	"crypto/rand"
 )
 
-func SignMessage(message string, privateKey string) (string, error) {
-	privateKeyECDSA, err := crypto.HexToECDSA(privateKey)
-	if err != nil {
-		return "", fmt.Errorf("invalid private key: %w", err)
-	}
-
-	messageHash := crypto.Keccak256Hash([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(message), message)))
-
-	signature, err := crypto.Sign(messageHash.Bytes(), privateKeyECDSA)
-	if err != nil {
-		return "", fmt.Errorf("failed to sign message: %w", err)
-	}
-
-	signature[64] += 27
-
-	return hexutil.Encode(signature), nil
-}
-
 func EncryptMessageForKeeper(publicKeyHex string, ipfsHost string, pinataJWT string) (string, error) {
 	publicKeyBytes, err := hexutil.Decode(fmt.Sprintf("0x%s", publicKeyHex))
 	if err != nil {
---
+++ pkg/encrypt/sign.go
@@ -0,0 +1,61 @@
+package encrypt
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func SignMessage(message string, privateKey string) (string, error) {
+	privateKeyECDSA, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("invalid private key: %w", err)
+	}
+
+	messageHash := crypto.Keccak256Hash([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(message), message)))
+
+	signature, err := crypto.Sign(messageHash.Bytes(), privateKeyECDSA)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign message: %w", err)
+	}
+
+	signature[64] += 27
+
+	return hexutil.Encode(signature), nil
+}
+
+func SignJSONMessage(jsonData interface{}, privateKey string) (string, error) {
+	jsonDataMap := make(map[string]interface{})
+	
+	jsonBytes, err := json.Marshal(jsonData)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal input data: %w", err)
+	}
+	
+	if err := json.Unmarshal(jsonBytes, &jsonDataMap); err != nil {
+		return "", fmt.Errorf("failed to unmarshal to map: %w", err) 
+	}
+
+	convertToLower(jsonDataMap)
+
+	jsonDataBytes, err := json.Marshal(jsonDataMap)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal json data: %w", err)
+	}
+
+	message := string(jsonDataBytes)
+
+	return SignMessage(message, privateKey)
+}
+
+func convertToLower(data map[string]interface{}) {
+	for k, v := range data {
+		if s, ok := v.(string); ok {
+			data[k] = strings.ToLower(s)
+		} else if m, ok := v.(map[string]interface{}); ok {
+			convertToLower(m)
+		}
+	}
+}
